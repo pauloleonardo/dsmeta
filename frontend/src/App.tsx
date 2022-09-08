8ada61e89cc208e9eef7eee637ff59a95e3dfaf6
Paulo Leonardo <paulo_leo90@hotmail.com>
2022-09-08 19:52:35 -0300

    First Component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,12 @@
+import NotificationButton from "./componentes/NotificationButton"
 
 
 function App() {
-  return(
-    <h1>Olá mundo</h1>
+  return (
+    <>
+      <h1>Olá mundo</h1>
+      <NotificationButton />
+    </>
   )
 }
 
